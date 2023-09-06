5ebe83491f5ab3e8e3f6d83a8dbe6852e598f283
Joel <brandonmoreles7@gmail.com>
2023-09-06 13:55:36 -0600

    Agregado visualizacion de categorias en noticias

--- src/components/News.js
+++ src/components/News.js
@@ -5,14 +5,21 @@ import imagen2 from "../assets/images/noticia2.jpeg"
 import imagen3 from "../assets/images/noticia3.jpeg"
 import imagen4 from "../assets/images/noticia2.png"
 import "../styles/news.css"
-
+import Menu from './Menu';
 
 const News = () => {
 
   return (
     <div className='news'>
-        <h6 className='titleNews'>Conoce las recientes</h6>
-        <h2 className='titleNews2'>Noticias del mundo juridico</h2>
+        <div className='headNews'>
+          <div>        
+            <h6 className='titleNews'>Conoce las recientes</h6>
+            <h2 className='titleNews2'>Noticias del mundo juridico</h2>
+          </div> 
+          <Menu/>
+        </div>
+          
+       
         <div className="cardContain"> 
             <Card Imagen={imagen1}texto={"El encabezado deberia de ir aqui"}/>
             <Card Imagen={imagen2}texto={"El encabezado deberia de ir aqui"}/>
